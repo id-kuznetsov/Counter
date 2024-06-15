9989071e7cb20640e99f29fff137a7ff0dcd2ff8
id-kuznetsov <ilua.kuznetsov@gmail.com>
2024-06-15 13:29:44 +0300

    Создан проект Counter

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Илья Кузнецов on 15.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
